36f3de1f39869f0c47931a9975412238a7abca73
Antti Toivanen <antti.toivanen@vikingtheories.com>
2018-11-07 12:07:57 +0200

    Remove extra forked stuff

--- index.js
+++ index.js
@@ -1,55 +1,6 @@
-import cors from 'cors';
-import {
-  getAlbums,
-  getPhotos,
-  validPassword,
-  createJWT,
-  validateAccess,
-} from './src';
+const rules = async (req, res) => {
+  return res.status(200).send()
+}
 
-const corsHandler = cors();
 
-const getter = {
-  album: getAlbums,
-  photo: getPhotos,
-};
-
-const getObjects = async (req, res, type) => {
-  const validJWT = validateAccess(req.headers);
-  if (!validJWT) {
-    return res.status(401).send('Access denied');
-  }
-  const result = await getter[type].call();
-  const json = JSON.stringify(result);
-  return res.status(200).send(json);
-};
-
-const albums = async (request, response) =>
-  corsHandler(request, response, () => {
-    getObjects(request, response, 'album');
-  });
-
-const photos = async (request, response) =>
-  corsHandler(request, response, () => {
-    getObjects(request, response, 'photo');
-  });
-
-const loginFn = (request, response) => {
-  if (request.method !== 'POST') {
-    return response.status(403).send('Forbidden!');
-  }
-  if (!validPassword(request.body.password)) {
-    return response.status(401).send('Wrong password');
-  }
-  const jwt = createJWT();
-  const json = JSON.stringify(jwt);
-  return response.status(200).send(json);
-};
-
-const login = (request, response) => {
-  corsHandler(request, response, () => {
-    loginFn(request, response);
-  });
-};
-
-export { albums, photos, login };
+export { rules };
--- src/dev.js
+++
@@ -1,8 +0,0 @@
-import { getAlbums } from '.';
-
-const run = async () => {
-  const albums = await getAlbums();
-  console.log('albums', albums[0].mainImage);
-};
-
-run();
