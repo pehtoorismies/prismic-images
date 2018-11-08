a7c494e1111bb099c2acdb1b207f233f98c2e479
Antti Toivanen <antti.toivanen@vikingtheories.com>
2018-11-08 12:17:42 +0200

    Parse the results into a more sensible structure

--- src/ruleparser.js
+++ src/ruleparser.js
@@ -1,3 +1,21 @@
-const parser = data => data;
+import * as R from 'ramda';
+
+const array2objects = data => {
+  const parsed = {};
+  let objectList;
+  R.keysIn(data).forEach(key => {
+    objectList = [];
+    R.range(1, data[key].length).forEach(idx => {
+      objectList.push(R.zipObj(data[key][0], data[key][idx]));
+    });
+    parsed[key] = objectList;
+  });
+  return parsed;
+};
+
+const parser = data => {
+  const objects = array2objects(data);
+  return objects;
+};
 
 export default parser;
